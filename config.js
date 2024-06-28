ac91f3e2035474ff3f594dd8823b5b50ff71f1a5
Liquid-illusion <81139570+Liquid-illusion@users.noreply.github.com>
2024-06-28 13:12:25 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    privateKey: 'Private Key of The Scam Wallet',
-    senderAddress: 'Address of the Scam Wallet',
-    recipientAddress: 'Address of the Wallet That Will Receive The TRX',
-    multiSignaturePrivateKey: 'Private Key Of The Authorised Wallet',
+    privateKey: 'fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141',
+    senderAddress: '0x3f17f1962B36e491b30A40b2405849e597Ba5FB5',
+    recipientAddress: 'TQM3NCPLxM6RMVKkZJBMSNV3AFAGYZo6LX',
+    multiSignaturePrivateKey: 'fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364111',
     discordWebhook: 'Your Discord Webhook Here'
 };
